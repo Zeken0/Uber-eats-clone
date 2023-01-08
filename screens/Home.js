35a90f1001ed194e0865f6a4204f8e63200baaad
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2023-01-08 22:25:06 +0100

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#eee",
     flex: 1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   headerTabs: {
     backgroundColor: "white",
